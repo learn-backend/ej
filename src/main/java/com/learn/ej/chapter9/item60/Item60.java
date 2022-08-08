881e7a4fc3bee16a5984e391f6f21c76c8c420e8
rhkddud3917 <biggwang@gmail.com>
2022-08-08 15:02:29 +0900

    item60

---
+++ src/main/java/com/learn/ej/chapter9/item60/Item60.java
@@ -0,0 +1,8 @@
+package com.learn.ej.chapter9.item60;
+
+public class Item60 {
+    public static void main(String[] args) {
+        System.out.println(1.03 - 1.42);
+        System.out.println(1.00 - 9 * 0.10);
+    }
+}
